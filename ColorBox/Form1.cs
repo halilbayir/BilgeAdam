1fdc75a5351294c0fca359d41514fb0ac05b5257
halilbayir <bayirhalil3@gmail.com>
2016-12-08 12:13:46 +0200

    upp

---
+++ ColorBox/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int red, green, blue;
+        private Color RastgeleRenkOlustur()
+        {
+            red = rnd.Next(0, 256);
+            green = rnd.Next(0, 256);
+            blue = rnd.Next(0, 256);
+
+            Color rastgeleRenk = Color.FromArgb(red, green, blue);
+            return rastgeleRenk;
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Button btn = new Button();
+                btn.Width = 30;
+                btn.Height = 30;
+                btn.Left = btn.Width * i;
+                btn.BackColor = RastgeleRenkOlustur();
+                btn.Click += ButtonClick;
+                
+                this.Controls.Add(btn);
+
+            }
+        }
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button secilenButon = sender as Button;
+            this.BackColor = secilenButon.BackColor;
+            
+        }
+
+    }
+}
